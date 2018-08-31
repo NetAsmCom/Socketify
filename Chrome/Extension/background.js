0859804479d27c8b27bb7cadcdc87510b32aa1a4
M. Fatih Marabaoglu <mfatihmar@gmail.com>
2018-08-31 14:01:02 +0100

    Implement Basic Background Handlers

---
+++ Chrome/Extension/background.js
@@ -0,0 +1,9 @@
+var nativePorts = {};
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    // TODO
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    // TODO
+});
