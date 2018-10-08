3b9a5c26d90fc3376b8f63d0b33aad5185f54100
M. Fatih MAR <mfatihmar@gmail.com>
2018-10-09 00:06:20 +0100

    Implement API Basics

--- Extensions/Chrome/socketify.js
+++ Extensions/Chrome/socketify.js
@@ -1,9 +1,24 @@
 window.socketify = {
     connect: function (encrypt) {
+        window.postMessage({
+            type: "socketify-outbound",
+            action: "connect",
+            encrypt: (typeof encrypt === typeof true) && encrypt
+        }, "*");
     },
     send: function (reliable, message) {
+        window.postMessage({
+            type: "socketify-outbound",
+            action: "send",
+            reliable: (typeof reliable === typeof true) && reliable,
+            message: message || {}
+        }, "*");
     },
     disconnect: function () {
+        window.postMessage({
+            type: "socketify-outbound",
+            action: "disconnect"
+        }, "*");
     },
     isConnected: false,
     isEncrypted: false,
@@ -17,5 +32,19 @@ window.addEventListener("message", function (event) {
         return;
     }
 
-    // TODO
+    switch (event.data.action) {
+        case "setProps":
+            window.socketify.isConnected = event.data.isConnected;
+            window.socketify.isEncrypted = event.data.isEncrypted;
+            break;
+        case "onConnect":
+            window.socketify.onConnect();
+            break;
+        case "onReceive":
+            window.socketify.onReceive(event.data.reliable, event.data.message);
+            break;
+        case "onDisconnect":
+            window.socketify.onDisconnect(event.data.error);
+            break;
+    }
 }, false);
