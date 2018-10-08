877b8e73504e746a5e7182639880ff6169f7e5a1
M. Fatih MAR <mfatihmar@gmail.com>
2018-10-09 00:30:46 +0100

    Implement Content Script Basics

--- Extensions/Chrome/content.js
+++ Extensions/Chrome/content.js
@@ -2,6 +2,8 @@ var script = document.createElement("script");
 script.src = chrome.extension.getURL("socketify.js");
 document.documentElement.appendChild(script);
 
+var isConnected = false;
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     // TODO
 });
@@ -11,5 +13,48 @@ window.addEventListener("message", function (event) {
         return;
     }
 
-    // TODO
+    switch (event.data.action) {
+        case "connect": {
+            if (isConnected) {
+                return;
+            }
+
+            if (!confirm("Do you want to allow this page to open encrypted connection?")) {
+                window.postMessage({
+                    type: "socketify-inbound",
+                    action: "error",
+                    error: {
+                        code: 1,
+                        text: "user refused permission to open connection"
+                    }
+                }, "*");
+                return;
+            }
+
+            chrome.runtime.sendMessage({
+                action: "connect",
+                encrypt: event.data.encrypt
+            });
+        } break;
+        case "send": {
+            if (!isConnected) {
+                return;
+            }
+
+            chrome.runtime.sendMessage({
+                action: "send",
+                reliable: event.data.reliable,
+                message: event.data.message
+            });
+        } break;
+        case "disconnect": {
+            if (!isConnected) {
+                return;
+            }
+
+            chrome.runtime.sendMessage({
+                action: "disconnect"
+            });
+        } break;
+    }
 }, false);
